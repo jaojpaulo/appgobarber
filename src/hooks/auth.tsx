6fb611a6697d2a96eb64acd4b7c7782982ec98f2
jaojpaulo <jaojpaulosantos@gmail.com>
2021-02-27 12:59:41 -0300

    Buscando providers e listagem de providers

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -70,7 +70,7 @@ const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
 
-    api.defaults.headers.authorization = `Bearer ${token[1]}`;
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
