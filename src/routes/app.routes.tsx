9dfd21c0be6eea53d1275864d32f6cb056bf97b7
jaojpaulo <jaojpaulosantos@gmail.com>
2021-02-27 16:56:48 -0300

    Corrigindo o comportamento do datepicker

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
+import AppointmentDatePicker from '../pages/AppointmentDatePicker';
 import AppointmentCreated from '../pages/AppointmentCreated';
 
 const App = createStackNavigator();
@@ -16,6 +17,10 @@ const AppRoutes: React.FC = () => (
   >
     <App.Screen name="Dashboard" component={Dashboard} />
     <App.Screen name="Profile" component={Profile} />
+    <App.Screen
+      name="AppointmentDatePicker"
+      component={AppointmentDatePicker}
+    />
     <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
   </App.Navigator>
 );
